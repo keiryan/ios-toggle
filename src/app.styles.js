a6deafc7bcf102ca60781eee5a396a9582f8b344
Keiryan Wilson <keiryanwilson@gmail.com>
2022-10-18 16:31:31 -0400

    added toggle functionality

---
+++ src/app.styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: black;
+`;
